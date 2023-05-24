f81bdd17956cb6413e807cf59d4be42b8bcb8c91
Sean OReilly <soreilly424@gmail.com>
2023-05-24 10:26:03 -0600

    debug screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
       blue: '#0f33fe',
       green: '#4cff99',
     },
+    debugScreens: {
+      position: ['bottom', 'right'],
+      style: {
+        backgroundColor: '#262626e6',
+        borderRadius: '4px',
+        boxShadow: 'none',
+        color: 'white',
+        padding: '8px',
+        margin: '18px 16px',
+        opacity: '100%',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-debug-screens')],
 }
